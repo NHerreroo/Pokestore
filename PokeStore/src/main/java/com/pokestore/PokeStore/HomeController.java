889e8b6019746582a415d5d1c28608e7314d7abc
d3iBiD23 <eddydavide.carrion.valarezo@elpuig.xeill.net>
2025-04-07 22:56:01 +0200

    Create HomeController.java

---
+++ PokeStore/src/main/java/com/pokestore/PokeStore/HomeController.java
@@ -0,0 +1,12 @@
+package com.pokestore.PokeStore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/cards/login";
+    }
+}
